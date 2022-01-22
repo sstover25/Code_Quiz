f199bdce3daffc27b973a6c1ffbe007462cafddf
SSto <95592688+sstover25@users.noreply.github.com>
2022-01-22 15:22:28 -0600

    added timer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,29 @@ var clearHighScoreBtnEl = document.querySelector("#clear-high-score-btn")
 var timerEl = document.querySelector("#timer")
 var quizStemSectionEl = document.querySelector("#quiz-stem")
 var highScoreListEl = document.querySelector("#high-score-list")
+var scoreEl = document.querySelector("#score")
 
 // start page
 
 // quiz page
+
+// starting the timer
+setInterval(startTimer, 1000);
+timerEl = 5000;
+function startTimer() {
+    if (timerEl > 0) {
+      timerEl = timerEl - 1000;
+      console.log(timerEl);
+    } else if (timerEl <=0) {
+        location.href = "./game-over.html"
+    }
+    
+}
+
+startTimer;
+
+// save score to localStorage
+
 
 // game over page
 
