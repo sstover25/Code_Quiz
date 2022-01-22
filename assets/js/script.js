4b2e7c9cd0794791d5c2cbf7a43a3a093b1bbd6f
SSto <95592688+sstover25@users.noreply.github.com>
2022-01-21 20:26:04 -0600

    set up html files and added variables to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 // variables for various buttons
 
+var quizOptionsSectionEl = document.querySelector("#quiz-options-selection")
+var submitBtnEl = document.querySelector("#submit-btn")
+var retryBtnEl = document.querySelector("#retry-btn")
+var clearHighScoreBtnEl = document.querySelector("#clear-high-score-btn")
+
+var timerEl = document.querySelector("#timer")
+var quizStemSectionEl = document.querySelector("#quiz-stem")
+var highScoreListEl = document.querySelector("#high-score-list")
+
 // start page
 
 // quiz page
