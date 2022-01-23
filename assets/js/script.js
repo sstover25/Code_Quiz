4a429bc67276c7ac4abb3e6c24f7b786ade0f6ec
SSto <95592688+sstover25@users.noreply.github.com>
2022-01-23 15:50:59 -0600

    functionality to pull random questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var retryBtnEl = document.querySelector("#retry-btn")
 var clearHighScoreBtnEl = document.querySelector("#clear-high-score-btn")
 
 
-var timerEl = 11
+var timerEl = 10
 
 
 var quizStemEl = document.querySelector("#quiz-stem")
@@ -22,7 +22,7 @@ var scoreEl = document.querySelector("#score")
 function startTimer() {
     timerEl = timerEl - 1;
     //if ()
-    if (timerEl < 11) {
+    if (timerEl < 10) {
         timer.innerHTML = timerEl;
     }
 
@@ -40,33 +40,51 @@ timerInterval = setInterval("startTimer()", 1000);
 // }
 var quizQuestions = [ {
     "question": "What command is used to create and move into a new branch?",
-    "options": ["checkout", "checkout -b", "add .", "add -A"],
-    "key": ["checkout -b"]
+    "options": "checkout",
+    "option2": "checkout -b", 
+    "option3": "add .", 
+    "option4": "add -A",
+    "key": "checkout -b"
 },
 {
     "question": "Which of the following special characters is often called a 'terminator'?",
-    "options": [";", ":", "*", "#"],
-    "key": [";"]
+    "options": ";",
+    "option2": ":", 
+    "option3": "*", 
+    "option4": "#",
+    "key": ";"
 },
 {
     "question": "What does the phrase 'declare a function' mean?",
-    "options": ["Deleting the function", "Appending the function", "Creating the function", "Displaying the function"],
-    "key": ["Creating the function"]
+    "options": "Deleting the function",
+    "option2": "Appending the function", 
+    "option3": "Creating the function", 
+    "option4": "Displaying the function",
+    "key": "Creating the function"
 },
 {
     "question": "What do you call the code written between two curly braces?",
-    "options": ["Code block", "Code bleak", "Code bland", "Code bloat"],
-    "key": ["Code block"]
+    "options": "Code block",
+    "option2": "Code blea", 
+    "option3": "Code bland", 
+    "option4": "Code bloat",
+    "key": "Code block"
 },
 {
     "question": "What code can we use to display something in the console?",
-    "options": ["console.call", "console.play", "console.cry", "console.log"],
-    "key": ["console.log"]
+    "option1": "console.call", 
+    "option2": "console.play", 
+    "option3": "console.cry", 
+    "option4": "console.log",
+    "key": "console.log"
 },
 {
     "question": "What special character should be used to see if the following values are equal in value and type: '2' __ 2?",
-    "options": ["!=", "==", "===", "="],
-    "key": ["==="]
+    "options": "!=",
+    "option2": "==",
+    "option3": "===",
+    "option4": "=",
+    "key": "==="
 }
 ]
 
@@ -75,9 +93,9 @@ var randomizeQuestion = Math.floor(Math.random() * quizQuestions.length);
 
 quizStemEl.innerHTML = quizQuestions[randomizeQuestion].question;
 
-for (var i = 0; i < maxQuestions; i++) {
-    quizOptionsEl[i].innerHTML = quizQuestions[randomizeQuestion].options[i];
-}
+// for (var i = 0; i < maxQuestions; i++) {
+//     quizOptionsEl[i].innerHTML = quizQuestions[randomizeQuestion].options[i];
+// }
 
 
 // save score to localStorage
